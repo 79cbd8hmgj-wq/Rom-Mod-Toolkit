62f1168071f2ccd3210508d5671a83efc3d80d9c
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-28 10:18:43 -0400

    test: enforce safe armips fragments

---
+++ tests/unit/test_armips.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import pytest
+
+from rommod.errors import ExternalToolError
+from rommod.platforms.nds.assembler import validate_armips_fragment
+
+
+@pytest.mark.parametrize(
+    "source",
+    [
+        '.open "other.bin",0x02000000',
+        '.create "new.bin",0',
+        '.close',
+        '.include "other.asm"',
+        '.headersize 0x10',
+        '.nds',
+        '.gba',
+    ],
+)
+def test_fragment_rejects_file_ownership_and_architecture_directives(source: str):
+    with pytest.raises(ExternalToolError, match="not allowed"):
+        validate_armips_fragment(source)
+
+
+def test_fragment_allows_arm_thumb_and_cpu_org():
+    validate_armips_fragment(".thumb\n.org 0x02000010\nnop\n.arm\n")
