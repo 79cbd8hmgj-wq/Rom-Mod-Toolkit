bb7b9fc6ebbe673a7ddebd1487e3cc14f4886d53
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-31 13:44:30 -0400

    feat: add reusable patching package

---
+++ src/rommod/patching/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable binary patch creation and verification."""
+
+from rommod.patching.distribution import PatchResult, create_project_patch, generate_binary_patch
+
+__all__ = ["PatchResult", "create_project_patch", "generate_binary_patch"]
